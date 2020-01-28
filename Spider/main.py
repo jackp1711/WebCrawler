bbbda64efb5e8a136fe2ff6738b753869e6cab1a
jackp1711 <jackyp98@hotmail.co.uk>
2020-01-28 18:01:30 +0000

    Will now accept user inputted URLs.
    TODO:
    * Stop all threads at max level
    * Create output file and print to console

--- Spider/main.py
+++ Spider/main.py
@@ -1,51 +1,16 @@
-import threading
-from queue import Queue
-from Spider import crawler
 from Spider import domain
-from Spider import general_functions as gf
+from Spider import crawler_manager
 
-PROJECT_NAME = 'YCombinator'
-HOMEPAGE = "https://news.ycombinator.com/"
+
+print("Thank you for running the WebCrawler application. Please enter the URL of the website you would like to crawl:")
+input1 = input()
+HOMEPAGE = str(input1)
 DOMAIN_NAME = domain.get_domain_name(HOMEPAGE)
-QUEUE_FILE = PROJECT_NAME + '/queue.txt'
-CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+PROJECT_NAME = DOMAIN_NAME
+
 MAX_URLS = 100
 NUMBER_OF_THREADS = 5
-thread_queue = Queue()
-crawler.Crawler(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME, MAX_URLS)
+print("Beginning crawl. Application will gather the first " + str(MAX_URLS) + " urls found")
 
-
-def create_jobs():
-    for link in gf.file_to_set(QUEUE_FILE):
-        thread_queue.put(link)
-    thread_queue.join()
-    crawl()
-
-
-def create_crawlers():
-    for _ in range(NUMBER_OF_THREADS):
-        t = threading.Thread(target=work)
-        t.daemon = True
-        t.start()
-
-
-def work():
-    while True:
-        url = thread_queue.get()
-        crawler.Crawler.crawl_page(threading.current_thread().name, url)
-        thread_queue.task_done()
-
-
-def crawl():
-    total_links = gf.get_number_of_urls_in_file(QUEUE_FILE) + gf.get_number_of_urls_in_file(CRAWLED_FILE)
-    if total_links >= MAX_URLS:
-        pass
-
-    queued_links = gf.file_to_set(QUEUE_FILE)
-    if len(queued_links) > 0:
-        print(str(len(queued_links)) + " links remaining in the queue")
-        create_jobs()
-
-
-create_crawlers()
-crawl()
+manager = crawler_manager.CrawlerManager(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME, MAX_URLS, NUMBER_OF_THREADS)
+manager.run()
